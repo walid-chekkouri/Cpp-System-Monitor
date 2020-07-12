831e15785bcf672c63ca415bcdae9f16eadf65e2
Walid_CHEKKOURI <virtualworld45@gmail.com>
2020-07-12 14:14:05 +0100

    Add Helper Function for ElapsedTime Calculation

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,36 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) 
+{ 
+  long currentResult = 0;
+  string elapsedTime {"00:00:00"};// HH:MM:SS format
+ 
+  if (seconds > 0) 
+  {
+    // Hours
+    currentResult = seconds / secondsInAnHour;
+    elapsedTime = TimeToStringConverter(currentResult) + ":";
+	
+    // Minutes
+    currentResult = (seconds / minutesInAnHour) % minutesInAnHour;
+    elapsedTime += TimeToStringConverter(currentResult) + ":";
+	
+    //Seconds
+    currentResult = seconds %  secondesInAnHour;
+   elapsedTime += TimeToStringConverter(currentResult);
+  }
+
+  return elapsedTime;
+
+ }
+ 
+ std::string Format::TimeToStringConverter(long time) 
+ {
+  if (time < 10)
+    return "0" + std::to_string(time);
+  else
+    return std::to_string(time);
+return (time < 10) ? "0" + std::to_string(time) : std::to_string(time);
+
+}
